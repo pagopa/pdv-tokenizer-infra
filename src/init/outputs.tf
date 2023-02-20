24ddf4e7ad4383717b16c174613f55e252a9c7ca
uolter <walter.traspadini@pagopa.it>
2023-02-20 12:26:42 +0100

    missed output with new role for IAC

--- src/init/outputs.tf
+++ src/init/outputs.tf
@@ -5,3 +5,8 @@ output "backend_bucket_name" {
 output "dynamodb_lock_table" {
   value = aws_dynamodb_table.dynamodb-terraform-state-lock.name
 }
+
+output "iac_role_arn" {
+  value       = aws_iam_role.githubiac.arn
+  description = "Role to use in github actions to build the infrastructure."
+}
