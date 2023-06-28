96f8dfb219fbaabfe1b5e4c5e7fb83f77379c3c2
uolter <walter.traspadini@pagopa.it>
2023-06-28 09:33:27 +0200

    update webacl count alarm

--- src/main/34-webacl.tf
+++ src/main/34-webacl.tf
@@ -116,9 +116,10 @@ module "webacl_count_alarm" {
   actions_enabled     = var.env_short == "p" ? true : false
   alarm_description   = "Alarm when webacl count greater than 10"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = 3
+  datapoints_to_alarm = 3
   threshold           = 10
-  period              = 60
+  period              = 300
   unit                = "Count"
 
   namespace   = "AWS/WAFV2"
