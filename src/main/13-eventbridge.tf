2cef80025056ec1084bc47212c69639e9188b231
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-07-24 15:45:21 +0200

    feat: Set glue crawler for count feature

--- src/main/13-eventbridge.tf
+++ src/main/13-eventbridge.tf
@@ -169,13 +169,7 @@ resource "aws_pipes_pipe" "token" {
 
   target_parameters {
     input_template = <<-EOT
-{
-    "eventID": <$.eventID>, 
-    "eventName": <$.eventName>, 
-    "SK": <$.dynamodb.NewImage.SK.S>,
-    "globalToken": <$.dynamodb.NewImage.globalToken.S>,
-    "token": <$.dynamodb.NewImage.token.S>
-}
+ { "eventID": <$.eventID>, "eventName": <$.eventName>, "SK": <$.dynamodb.NewImage.SK.S>, "globalToken": <$.dynamodb.NewImage.globalToken.S>, "token": <$.dynamodb.NewImage.token.S>}
 EOT
   }
 }
@@ -218,5 +212,119 @@ resource "aws_kinesis_firehose_delivery_stream" "firehose" {
     }
 
     file_extension = ".json"
+  }
+}
+
+module "s3_athena_output_bucket" {
+  count   = var.create_event_bridge_pipe ? 1 : 0
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.1"
+
+  bucket = format("bucket-%s-tokens-query", var.env_short)
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  tags = {
+    Name = format("bucket-%s-tokens-query", var.env_short)
+  }
+}
+
+resource "aws_athena_workgroup" "tokens_workgroup" {
+  count = var.create_event_bridge_pipe ? 1 : 0
+  name  = "tokens_workgroup"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${module.s3_athena_output_bucket[0].s3_bucket_id}/output/"
+    }
+  }
+}
+
+# Create Athena database
+resource "aws_athena_database" "tokens" {
+  count  = var.create_event_bridge_pipe ? 1 : 0
+  name   = "tokens_db"
+  bucket = module.s3_athena_output_bucket[0].s3_bucket_id
+}
+
+data "aws_iam_policy_document" "glue_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["glue.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "glue_tokens" {
+  count              = var.create_event_bridge_pipe ? 1 : 0
+  name               = "AWSGlueServiceRole-Tokens-role"
+  assume_role_policy = data.aws_iam_policy_document.glue_assume_role_policy.json
+  path               = "/service-role/"
+}
+
+data "aws_iam_policy_document" "glue_tokens_policy" {
+  statement {
+    sid       = "S3ReadAndWrite"
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::${module.s3_tokens_bucket[0].s3_bucket_id}/*"]
+    actions   = ["s3:GetObject", "s3:PutObject"]
+  }
+}
+
+resource "aws_iam_policy" "glue_tokens_policy" {
+  count       = var.create_event_bridge_pipe ? 1 : 0
+  name        = "AWSGlueServiceRoleTokensS3Policy"
+  description = "S3 bucket tokens policy for glue."
+  policy      = data.aws_iam_policy_document.glue_tokens_policy.json
+}
+
+locals {
+  glue_tokens_policy = compact([
+    "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole",
+    var.create_event_bridge_pipe ? aws_iam_policy.glue_tokens_policy[0].arn : null
+  ])
+}
+
+resource "aws_iam_role_policy_attachment" "glue_s3_tokens_policy" {
+  count      = var.create_event_bridge_pipe ? length(local.glue_tokens_policy) : 0
+  role       = aws_iam_role.glue_tokens[0].name
+  policy_arn = local.glue_tokens_policy[count.index]
+  depends_on = [aws_iam_policy.glue_tokens_policy]
+}
+
+resource "aws_glue_catalog_database" "tokens" {
+  count = var.create_event_bridge_pipe == true ? 1 : 0
+  name  = "tokens_catalog_db"
+}
+
+resource "aws_glue_crawler" "tokens" {
+  count         = var.create_event_bridge_pipe == true ? 1 : 0
+  database_name = aws_glue_catalog_database.tokens[0].name
+  name          = "tokens_crawler"
+  role          = aws_iam_role.glue_tokens[0].arn
+
+  #schedule = var.tokens_crawler_schedule
+
+  description = "Crawler for the tokens bucket"
+
+  configuration = jsonencode(
+    {
+      CrawlerOutput = {
+        Tables = {
+          TableThreshold = 1
+        }
+      }
+      CreatePartitionIndex = true
+      Version              = 1.0
+    }
+  )
+
+  s3_target {
+    path = "s3://${module.s3_tokens_bucket[0].s3_bucket_id}/tokens/"
   }
 }
