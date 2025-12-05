f6d85dd1348ad2fbf1aa6eb22e89fc3ef1bafe12
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2025-12-05 12:22:04 +0100

    fix: set correct condition

--- src/main/13-eventbridge.tf
+++ src/main/13-eventbridge.tf
@@ -298,17 +298,29 @@ resource "aws_iam_policy" "glue_tokens_policy" {
   policy      = data.aws_iam_policy_document.glue_tokens_policy[0].json
 }
 
+# locals {
+#   glue_tokens_policy = compact([
+#     "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole",
+#     var.create_event_bridge_pipe ? aws_iam_policy.glue_tokens_policy[0].arn : null
+#   ])
+# }
 locals {
-  glue_tokens_policy = compact([
-    "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole",
-    var.create_event_bridge_pipe ? aws_iam_policy.glue_tokens_policy[0].arn : null
-  ])
+  # List of mandatory policies (always attached if the feature is enabled)
+  glue_base_policies = ["arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"]
+
+  # List of ALL policies to attach if the feature is enabled
+  glue_tokens_policy_arns = compact(concat(
+    local.glue_base_policies,
+    var.create_event_bridge_pipe ? [aws_iam_policy.glue_tokens_policy[0].arn] : []
+  ))
 }
 
 resource "aws_iam_role_policy_attachment" "glue_s3_tokens_policy" {
-  count      = var.create_event_bridge_pipe ? length(local.glue_tokens_policy) : 0
+  count      = var.create_event_bridge_pipe ? length(local.glue_tokens_policy_arns) : 0
+  
   role       = aws_iam_role.glue_tokens[0].name
-  policy_arn = local.glue_tokens_policy[count.index]
+  policy_arn = local.glue_tokens_policy_arns[count.index] 
+  
   depends_on = [aws_iam_policy.glue_tokens_policy]
 }
 
