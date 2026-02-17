738047f0852d36f0cff137150ae3b44b8ca01505
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-02-17 17:38:36 +0100

    fix: Query sql for ingestion count

--- src/lambda/ingestion_count/query.sql
+++ src/lambda/ingestion_count/query.sql
@@ -1,6 +1,7 @@
-SELECT partition_0, year, month, day, count(*) AS daily_count FROM "{{db}}"."{{table}}"
+SELECT partition_0, year, month, day, count(eventid) AS daily_count FROM "{{db}}"."{{table}}"
 WHERE 
     year = ? 
     AND month = ? 
     AND day = ?
+    AND eventid IS NOT NULL
 GROUP BY partition_0, year, month, day
